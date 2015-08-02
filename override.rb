762037a9fe8785959a498ec2c6ebf77bc58b4ba3
Lev Kravinsky <kravinskylev@gmail.com>
2015-08-02 00:18:32 -0600

    initial pass working

---
+++ override.rb
@@ -0,0 +1,27 @@
+class SomeClass
+  def my_method
+    puts "foo"   # => nil, nil
+  end
+end
+
+a = SomeClass.new  # => #<SomeClass:0x007fda6b004d10>
+a.my_method        # => nil
+
+#in the test suite, this is the stubbed singleton method
+#__________________________________________________________
+copy = a.method(:my_method)                                  # => #<Method: SomeClass#my_method>
+a.send(:define_singleton_method, :my_method) { puts "bar" }  # => :my_method
+#__________________________________________________________
+
+a.my_method  # => nil
+
+#this is the reversion back to the old method
+#__________________________________________________
+a.define_singleton_method(:my_method) { copy.call }  # => :my_method
+#__________________________________________________
+
+a.my_method  # => nil
+
+# >> foo
+# >> bar
+# >> foo
